46643e6193a44cf82d9e823d792d501a7a013460
Brock T. Bennett <brock.bennett@okstate.edu>
2023-11-17 12:26:52 -0600

    Completed module 10 questions in Canvas. Uploaded to github on Oct 4, 2023.

---
+++ Module 10: Web Services (API Connection)/02API, Post and Get Data/postTweets.py
@@ -0,0 +1,30 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+# Update the path to where your .env file is located
+load_dotenv("/Users/brocktbennett/GitHub/MSIS-5193-Data-Science-Lab-Work/Module 10: Web Services (API Connection)/gitignore/.env")  # Adjust this path to your .env file's location
+
+# Use environment variables
+api_key = os.getenv('API_KEY')
+api_secret = os.getenv('API_SECRET')  # Renamed from api_secrets for consistency
+access_token = os.getenv('ACCESS_TOKEN')
+access_secret = os.getenv('ACCESS_SECRET')
+bearer_token = os.getenv("BEARER_TOKEN")
+
+
+
+# V2 Twitter API Authentication
+client = tweepy.Client(
+    bearer_token,
+    api_key,
+    api_secret,
+    access_token,
+    access_secret,
+    wait_on_rate_limit=True,
+)
+
+
+client.create_tweet(text="It is Friday again!")
+
